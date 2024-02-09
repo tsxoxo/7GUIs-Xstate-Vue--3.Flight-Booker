71c71114c4f4cd50d365c95e6e831648b9c173d8
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2024-02-09 12:09:45 +0000

    style: add macro layout

--- src/FlightBooker.vue
+++ src/FlightBooker.vue
@@ -14,29 +14,26 @@ const { snapshot, send } = useMachine(flightMachine, {
 </script>
 
 <template>
-  <div class="center-children">
-    <label for="pet-select">Flight Type</label>
+  <div class="center-children" id="select-and-inputs">
+    <!-- <label for="pet-select">Flight Type</label> -->
     <select @change="(event) => send({ type: 'changeFlightType', value: (event!.target as HTMLInputElement)!.value })"
       name="pets" id="pet-select" size="2">
       <option :selected="snapshot.context.flightType === 'one-way'" value="one-way">One-Way</option>
       <option :selected="snapshot.context.flightType === 'return'" value="return">Return</option>
     </select>
-  </div>
-  <div class="center-children">
-    <label for="start-date">Start Date</label>
+    <!-- <label for="start-date">Start Date</label> -->
     <input :class="{ invalidDateInput: !snapshot.context.startDate.isValid }"
       @input="(event) => send({ type: 'changeStartDate', value: (event!.target as HTMLInputElement)!.value })"
       :value="snapshot.context.startDate.date" type="text" id="start-date" required minlength="6" maxlength="10"
       size="10" />
-  </div>
-  <div class="center-children">
-    <label for="return-date">Return Date</label>
+    <!-- <label for="return-date">Return Date</label> -->
     <input :disabled="snapshot.context.flightType === 'one-way'"
       :class="{ invalidDateInput: snapshot.context.flightType !== 'one-way' && !snapshot.context.returnDate.isValid }"
       @input="(event) => send({ type: 'changeReturnDate', value: (event!.target as HTMLInputElement)!.value })"
       :value="snapshot.context.returnDate.date" type="text" id="return-date" required minlength="6" maxlength="10"
       size="10" />
   </div>
-  <button :disabled="!snapshot.context.canBook" @click="send({ type: 'book' })"> Book flight!
+  <button :disabled="!snapshot.context.canBook" @click="send({ type: 'book' })">
   </button>
+  <p id="button-label">Book flight</p>
 </template>
