7f64e65212e5c51a281725e6ffb9081de9b2d75d
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2024-02-12 10:31:31 +0000

    a11y: add `aria-label` for date inputs

--- src/FlightBooker.vue
+++ src/FlightBooker.vue
@@ -32,18 +32,14 @@ const returnDate = toRef(() => snapshot.value.context.returnDate);
         <label for="return">Return</label>
       </div>
     </div>
-    <!-- #TODO invisible labels? -->
-    <!-- <label for="pet-select">Flight Type</label> -->
-    <!-- <label for="start-date">Start Date</label> -->
     <div class="input-wrapper">
-      <input :class="{ 'invalid-date-input': errors.has('changeStartDate-invalid') }"
+      <input aria-label="start date" :class="{ 'invalid-date-input': errors.has('changeStartDate-invalid') }"
         @input="(event) => send({ type: 'changeStartDate', value: (event!.target as HTMLInputElement)!.value })"
         :value="startDate" type="text" id="start-date" required minlength="6" maxlength="10" size="10" />
       <span :class="{ 'show': errors.has('dates-incompatible') }" class="date-conflict-indicator">▼</span>
     </div>
-    <!-- <label for="return-date">Return Date</label> -->
     <div class="input-wrapper">
-      <input :disabled="flightType !== 'return'"
+      <input aria-label="return date" :disabled="flightType !== 'return'"
         :class="{ 'invalid-date-input': flightType === 'return' && errors.has('changeReturnDate-invalid') }"
         @input="(event) => send({ type: 'changeReturnDate', value: (event!.target as HTMLInputElement)!.value })"
         :value="returnDate" type="text" id="return-date" required minlength="6" maxlength="10" size="10" />
